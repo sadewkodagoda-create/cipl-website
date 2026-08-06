034259696aeb1e48b36b65be01444085d19d374c
sadewkodagoda-create <294427946+sadewkodagoda-create@users.noreply.github.com>
2026-08-06 08:10:33 +0530

    Restore CIPL website

--- api/maintenance.js
+++
@@ -1,4 +0,0 @@
-export default function handler(_request, response) {
-  response.setHeader("Cache-Control", "no-store, max-age=0");
-  response.status(503).send("Service temporarily unavailable");
-}
